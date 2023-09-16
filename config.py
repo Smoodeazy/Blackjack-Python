7b94d45e224052b0daa1c3f897028767087c8ec5
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-09-16 17:00:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 cardSym: list = ["Spades", "Clubs", "Hearts", "Diamonds"]
 faceCards: list = ["King", "Queen", "Jack", "Ace"]
-
+invalid: str = "Invalid Input. Please try again"
 values: list = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
